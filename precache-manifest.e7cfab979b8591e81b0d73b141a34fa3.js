72e41025094d4e3c683216afa9bd827a64d75551
Samuli Löf <samuli.lof@gmail.com>
2020-05-15 21:02:45 +0300

    Updates

---
+++ precache-manifest.e7cfab979b8591e81b0d73b141a34fa3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ffeb710ffb7fd318c54940c9df0aa6b",
+    "url": "/novel-reader/index.html"
+  },
+  {
+    "revision": "3532996ecfc893395c36",
+    "url": "/novel-reader/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3ed977486b2e812d2c83",
+    "url": "/novel-reader/static/js/2.99882819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/novel-reader/static/js/2.99882819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3532996ecfc893395c36",
+    "url": "/novel-reader/static/js/main.5ccfe6d2.chunk.js"
+  },
+  {
+    "revision": "91b9d0e26b9452ccc0dc",
+    "url": "/novel-reader/static/js/runtime-main.1ee4e9b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/novel-reader/static/media/logo.5d5d9eef.svg"
+  }
+]);
